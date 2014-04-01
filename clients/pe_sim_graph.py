cc2f2b61106856f341f9f307beac5ff259a0a770
Brian Wylie <briford.wylie@gmail.com>
2014-03-31 23:48:57 -0600

    fix bad cut/paste error
    Former-commit-id: 01d8a98bc6ff9760ffa4a2fef3bebffb957873ea

--- clients/pe_sim_graph.py
+++ clients/pe_sim_graph.py
@@ -45,7 +45,7 @@ def main():
     c = zerorpc.Client(timeout=600)
     c.connect('tcp://127.0.0.1:'+port)
 
-    '''
+
     # Test out PEFile -> pe_deep_sim -> pe_jaccard_sim -> graph
     bad_files = [os.path.join('../test_files/pe/bad', child) for child in os.listdir('../test_files/pe/bad')]
     good_files = [os.path.join('../test_files/pe/good', child) for child in os.listdir('../test_files/pe/good')]
@@ -62,7 +62,6 @@ def main():
     for result in results:
         for sim_info in result['sim_list']:
             c.add_rel(result['md5'], sim_info['md5'], 'ssdeep')
-    '''
 
     # Compute pe_features on all files of type pe, just pull back the sparse features
     results = c.batch_work_request('pe_features', {'type_tag': 'pe', 'subkeys':['md5','sparse_features.imported_symbols']})
@@ -88,9 +87,9 @@ def main():
     for sim_info in sims:
         c.add_rel(sim_info['source'], sim_info['target'], 'warnings')
 
-    # Compute pe_peid on all files of type pe, just pull back the match_list
-    results = c.batch_work_request('pe_peid', {'type_tag': 'pe', 'subkeys':['md5','match_list']})
 
+    # Compute strings on all files of type pe, just pull back the string_list
+    results = c.batch_work_request('strings', {'type_tag': 'pe', 'subkeys':['md5','string_list']})
 
     # Split out the md5 list and feature list
     md5_list = [r['md5'] for r in results]
@@ -105,6 +104,8 @@ def main():
     ''' PEiD takes a long time so commenting out for now '''
 
     '''
+    # Compute pe_peid on all files of type pe, just pull back the match_list
+    results = c.batch_work_request('pe_peid', {'type_tag': 'pe', 'subkeys':['md5','match_list']})
 
     # Split out the md5 list and feature list
     md5_list = [r['md5'] for r in results]
@@ -115,8 +116,6 @@ def main():
     for sim_info in sims:
         c.add_rel(sim_info['source'], sim_info['target'], 'peid')
 
-    # Compute strings on all files of type pe, just pull back the string_list
-    results = c.batch_work_request('strings', {'type_tag': 'pe', 'subkeys':['md5','string_list']})
     '''
 
 def test():
