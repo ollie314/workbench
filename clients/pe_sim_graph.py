8f51617d6b750fc9fff6f9120b777ea54218f581
Brian Wylie <briford.wylie@gmail.com>
2014-03-31 23:08:40 -0600

    some fixing/cleanup of pe_sim_graph client
    Former-commit-id: 427adc152f2ed821c3d1eb5c026aaa7bf5169401

--- clients/pe_sim_graph.py
+++ clients/pe_sim_graph.py
@@ -42,9 +42,10 @@ def main():
     parser.add_argument('-p', '--port', type=int, default=4242, help='port used by workbench server')
     args = parser.parse_args()
     port = str(args.port)
-    c = zerorpc.Client(timeout=300)
+    c = zerorpc.Client(timeout=600)
     c.connect('tcp://127.0.0.1:'+port)
 
+    '''
     # Test out PEFile -> pe_deep_sim -> pe_jaccard_sim -> graph
     bad_files = [os.path.join('../test_files/pe/bad', child) for child in os.listdir('../test_files/pe/bad')]
     good_files = [os.path.join('../test_files/pe/good', child) for child in os.listdir('../test_files/pe/good')]
@@ -53,6 +54,7 @@ def main():
     bad_md5s = add_it(c, bad_files, ['pe','bad'])
     good_md5s = add_it(c, good_files, ['pe','good'])
 
+
     # Compute pe_deep_sim on all files of type pe
     results = c.batch_work_request('pe_deep_sim', {'type_tag': 'pe'})
 
@@ -60,6 +62,7 @@ def main():
     for result in results:
         for sim_info in result['sim_list']:
             c.add_rel(result['md5'], sim_info['md5'], 'ssdeep')
+    '''
 
     # Compute pe_features on all files of type pe, just pull back the sparse features
     results = c.batch_work_request('pe_features', {'type_tag': 'pe', 'subkeys':['md5','sparse_features.imported_symbols']})
@@ -80,7 +83,7 @@ def main():
     md5_list = [r['md5'] for r in results]
     feature_list = [r['pe_warning_strings'] for r in results]
 
-    # Compute the Jaccard Index between imported systems and store as relationships
+    # Compute the Jaccard Index between warnings and store as relationships
     sims = jaccard_sims(md5_list, feature_list)
     for sim_info in sims:
         c.add_rel(sim_info['source'], sim_info['target'], 'warnings')
@@ -88,26 +91,33 @@ def main():
     # Compute pe_peid on all files of type pe, just pull back the match_list
     results = c.batch_work_request('pe_peid', {'type_tag': 'pe', 'subkeys':['md5','match_list']})
 
+
+    # Split out the md5 list and feature list
+    md5_list = [r['md5'] for r in results]
+    feature_list = [r['string_list'] for r in results]
+
+    # Compute the Jaccard Index between strings and store as relationships
+    sims = jaccard_sims(md5_list, feature_list)
+    for sim_info in sims:
+        c.add_rel(sim_info['source'], sim_info['target'], 'strings')
+
+    
+    ''' PEiD takes a long time so commenting out for now '''
+
+    '''
+
     # Split out the md5 list and feature list
     md5_list = [r['md5'] for r in results]
     feature_list = [r['match_list'] for r in results]
 
-    # Compute the Jaccard Index between imported systems and store as relationships
+    # Compute the Jaccard Index between peid sigs and store as relationships
     sims = jaccard_sims(md5_list, feature_list)
     for sim_info in sims:
         c.add_rel(sim_info['source'], sim_info['target'], 'peid')
 
     # Compute strings on all files of type pe, just pull back the string_list
     results = c.batch_work_request('strings', {'type_tag': 'pe', 'subkeys':['md5','string_list']})
-
-    # Split out the md5 list and feature list
-    md5_list = [r['md5'] for r in results]
-    feature_list = [r['string_list'] for r in results]
-
-    # Compute the Jaccard Index between imported systems and store as relationships
-    sims = jaccard_sims(md5_list, feature_list)
-    for sim_info in sims:
-        c.add_rel(sim_info['source'], sim_info['target'], 'strings')
+    '''
 
 def test():
     ''' pe_peid test '''
