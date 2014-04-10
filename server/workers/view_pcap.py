ef4ef5863130f859f8013cfd0c07a61d882e0548
Brian Wylie <briford.wylie@gmail.com>
2014-04-10 13:44:08 -0600

    adding a pcap details view
    Former-commit-id: 69854b632bd9b1dcc1a56d6204653c77fa38c042

--- server/workers/view_pcap.py
+++ server/workers/view_pcap.py
@@ -1,14 +1,14 @@
-''' view_pcap_bro worker '''
+''' view_pcap worker '''
 import zerorpc
 import itertools
 
 
 def plugin_info():
-    return {'name':'view_pcap_bro', 'class':'ViewPcapBro', 'dependencies': ['pcap_bro', 'pcap_meta'],
-            'description': 'This worker generates a pcap view for the sample. Output keys: [bro_output_log_names...]'}
+    return {'name':'view_pcap', 'class':'ViewPcap', 'dependencies': ['pcap_bro', 'pcap_meta'],
+            'description': 'This worker generates a pcap view. Output keys: [bro_logs, extracted_files, ...]'}
 
-class ViewPcapBro():
-    ''' ViewPcapBro: Generates a view for bro output on a pcap sample '''
+class ViewPcap():
+    ''' ViewPcap: Generates a view for a pcap sample (depends on Bro)'''
     def __init__(self):
         self.c = zerorpc.Client()
         self.c.connect("tcp://127.0.0.1:4242")
@@ -16,16 +16,14 @@ class ViewPcapBro():
     def execute(self, input_data):
 
         # Loop around the output keys for pcap_meta and pcap_bro output
-        view = {key: input_data['pcap_bro'][key] for key in input_data['pcap_bro'].keys()}
-        view.update({key: input_data['pcap_meta'][key] for key in input_data['pcap_meta'].keys()})
+        view = {key: input_data['pcap_meta'][key] for key in input_data['pcap_meta'].keys()}
 
-        # Okay this view is going to also take a peek at the bro output logs
-        for name, md5 in input_data['pcap_bro'].iteritems():
-            if '_log' in name:
-                view[name] = []
-                stream = self.c.stream_sample(md5, 20)
-                for row in itertools.islice(stream, 0, 1):
-                    view[name].append(row)
+        # Grab logs from Bro
+        view['bro_logs'] = {key: input_data['pcap_bro'][key] for key in input_data['pcap_bro'].keys() if '_log' in key}
+
+        # Grab logs from Bro
+        view['extracted_files'] = input_data['pcap_bro']['extracted_files']
+
         return view
 
     def __del__(self):
@@ -35,15 +33,15 @@ class ViewPcapBro():
 
 # Unit test: Create the class, the proper input and run the execute() method for a test
 def test():
-    ''' view_pcap_bro.py: Unit test'''
+    ''' view_pcap.py: Unit test'''
     # This worker test requires a local server as it relies on the recursive dependencies
     import zerorpc
     c = zerorpc.Client(timeout=300)
     c.connect("tcp://127.0.0.1:4242")
 
-    md5 = c.store_sample('http.pcap', open('../../test_files/pcap/http.pcap', 'rb').read(), 'pcap')
-    output = c.work_request('view_pcap_bro', md5)
-    print 'ViewPcapBro: '
+    md5 = c.store_sample('http.pcap', open('../../test_files/pcap/winmediaplayer.pcap', 'rb').read(), 'pcap')
+    output = c.work_request('view_pcap', md5)
+    print 'ViewPcap: '
     import pprint
     pprint.pprint(output)
 
---
+++ server/workers/view_pcap_details.py
@@ -0,0 +1,70 @@
+''' view_pcap_details worker '''
+import zerorpc
+import itertools
+import view_pcap
+
+
+def plugin_info():
+    return {'name':'view_pcap_details', 'class':'ViewPcapDetails', 'dependencies': ['view_pcap'],
+            'description': 'This worker generates a detailed pcap view. Output keys: [...]'}
+
+class ViewPcapDetails():
+    ''' ViewPcapDetails: Generates a view for a pcap sample (depends on Bro)'''
+    def __init__(self):
+        self.c = zerorpc.Client()
+        self.c.connect("tcp://127.0.0.1:4242")
+
+    def execute(self, input_data):
+        
+        # Copy info from input
+        view = input_data['view_pcap']
+        
+        # Grab a couple of handles
+        extracted_files = input_data['view_pcap']['extracted_files']
+        bro_logs = input_data['view_pcap']['bro_logs']
+        
+        # Dump a couple of fields
+        del view['encoding']
+        del view['extracted_files']        
+
+        # Gather additional info from the Bro logs
+        '''
+        view['bro_log_meta'] = [self.c.work_request('meta', md5) for md5 in bro_logs]
+        '''
+
+        # Grab additional info about the extracted files
+        view['extracted_files'] = [self.c.work_request('meta_deep', md5) for md5 in extracted_files]
+
+
+        '''
+        # Okay this view is going to also take a peek at the bro output logs
+        for name, md5 in input_data['pcap_bro'].iteritems():
+            if '_log' in name:
+                view[name] = []
+                stream = self.c.stream_sample(md5, 20)
+                for row in itertools.islice(stream, 0, 1):
+                    view[name].append(row)
+        '''
+        return view
+
+    def __del__(self):
+        ''' Class Cleanup '''
+        # Close zeroRPC client
+        self.c.close()
+
+# Unit test: Create the class, the proper input and run the execute() method for a test
+def test():
+    ''' view_pcap_details.py: Unit test'''
+    # This worker test requires a local server as it relies on the recursive dependencies
+    import zerorpc
+    c = zerorpc.Client(timeout=300)
+    c.connect("tcp://127.0.0.1:4242")
+
+    md5 = c.store_sample('http.pcap', open('../../test_files/pcap/winmediaplayer.pcap', 'rb').read(), 'pcap')
+    output = c.work_request('view_pcap_details', md5)
+    print 'ViewPcapDetails: '
+    import pprint
+    pprint.pprint(output)
+
+if __name__ == "__main__":
+    test()
