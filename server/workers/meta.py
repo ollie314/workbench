feac25e3174e5da2694a8e2dc379c40f89fe13dc
Brian Wylie <briford.wylie@gmail.com>
2014-05-17 13:03:59 -0600

    pylint
    Former-commit-id: ba71a182683061abc0843e05de4b9e5459de35d6

--- server/workers/meta.py
+++ server/workers/meta.py
@@ -4,28 +4,32 @@ import hashlib
 import magic
 import datetime
 
-class MetaData():
+class MetaData(object):
     ''' This worker computes meta data for any file type. '''
     dependencies = ['sample']
 
-    def execute(self, input_data):
-        raw_bytes = input_data['sample']['raw_bytes']
-        md5 = hashlib.md5(raw_bytes).hexdigest()
-        type_tag = input_data['sample']['type_tag']
-        with magic.Magic() as m:
-            file_type = m.id_buffer(raw_bytes[:1024])
-        with magic.Magic(flags=magic.MAGIC_MIME_TYPE) as m:
-            mime_type = m.id_buffer(raw_bytes[:1024])
-        with magic.Magic(flags=magic.MAGIC_MIME_ENCODING) as m:
-            encoding = m.id_buffer(raw_bytes[:1024])
-        file_size = len(raw_bytes)
-        filename = input_data['sample']['filename']
-        import_time = input_data['sample']['import_time']
-        customer = input_data['sample']['customer']
-        length = input_data['sample']['length']
+    def __init__(self):
+        ''' Initialization '''
+        self.meta = {}
 
-        return {name:value for name,value in locals().iteritems()
-                if name not in ['self', 'input_data','raw_bytes', 'm']}
+    def execute(self, input_data):
+        ''' This worker computes meta data for any file type. '''
+        raw_bytes = input_data['sample']['raw_bytes']
+        self.meta['md5'] = hashlib.md5(raw_bytes).hexdigest()
+        self.meta['type_tag'] = input_data['sample']['type_tag']
+        with magic.Magic() as mag:
+            self.meta['file_type'] = mag.id_buffer(raw_bytes[:1024])
+        with magic.Magic(flags=magic.MAGIC_MIME_TYPE) as mag:
+            self.meta['mime_type'] = mag.id_buffer(raw_bytes[:1024])
+        with magic.Magic(flags=magic.MAGIC_MIME_ENCODING) as mag:
+            self.meta['encoding'] = mag.id_buffer(raw_bytes[:1024])
+        self.meta['file_size'] = len(raw_bytes)
+        self.meta['filename'] = input_data['sample']['filename']
+        self.meta['import_time'] = input_data['sample']['import_time']
+        self.meta['customer'] = input_data['sample']['customer']
+        self.meta['length'] = input_data['sample']['length']
+
+        return self.meta
 
 
 # Unit test: Create the class, the proper input and run the execute() method for a test
@@ -35,8 +39,8 @@ def test():
 
     import pprint
     pprint.pprint(worker.execute({'sample':{'raw_bytes':open('../../data/pe/bad/033d91aae8ad29ed9fbb858179271232', 'rb').read(),
-                    'length':0, 'filename': 'bad_033d91', 'type_tag': 'pe','customer':'MegaCorp',
-                    'import_time':datetime.datetime.now()}}))
+                                            'length':0, 'filename': 'bad_033d91', 'type_tag': 'pe', 'customer':'MegaCorp',
+                                            'import_time':datetime.datetime.now()}}))
 
 if __name__ == "__main__":
     test()
