704a0b6a5beea2f364f5000f8db87056da2e2d8c
Brian Wylie <briford.wylie@gmail.com>
2014-05-19 12:26:20 -0600

    refactoring both output and tests
    Former-commit-id: d345ee8249a2fa8665d9f10d33a6f8e27324f6d7

--- server/workers/log_meta.py
+++ server/workers/log_meta.py
@@ -7,39 +7,44 @@ class LogMetaData():
     ''' This worker computes a meta-data for log files. '''
     dependencies = ['sample', 'meta']
 
+    def __init__(self):
+        ''' Initialization '''
+        self.meta = {}
+
     def execute(self, input_data):
         raw_bytes = input_data['sample']['raw_bytes']
-        num_rows = raw_bytes.count('\n')
-        head = raw_bytes[:100]
-        tail = raw_bytes[-100:]
-        output = {name:value for name,value in locals().iteritems()
-                if name not in ['self', 'input_data','raw_bytes']}
-        output.update(input_data['meta'])
-        return output
+        self.meta['num_rows'] = raw_bytes.count('\n')
+        self.meta['head'] = raw_bytes[:100]
+        self.meta['tail'] = raw_bytes[-100:]
+        self.meta.update(input_data['meta'])
+        return self.meta
 
 
 # Unit test: Create the class, the proper input and run the execute() method for a test
 def test():
     ''' log_meta.py: Unit test'''
+    # This worker test requires a local server as it relies heavily on the recursive dependencies
+    import zerorpc
+    c = zerorpc.Client()
+    c.connect("tcp://127.0.0.1:4242")
 
-    # Grab a sample
-    sample = {'sample':{'raw_bytes':open('../../data/log/system.log', 'rb').read(), 'length':0,
-              'filename': 'system.log', 'type_tag': 'pe', 'customer':'MegaCorp',
-              'import_time':datetime.datetime.now()}}
+    # Generate input for the worker
+    md5 = c.store_sample('bad_067b392', open('../../data/log/system.log', 'rb').read(), 'log')
+    input_data = c.get_sample(md5)
+    input_data.update(c.work_request('meta', md5))
 
-    # Send it through meta
-    import meta
-    input_worker = meta.MetaData()
-    _raw_output = input_worker.execute(sample)
-    wrapped_output = {'meta':_raw_output}
-
-    # Now join up the inputs
-    wrapped_output.update(sample)
-
+    # Execute the worker (unit test)
     worker = LogMetaData()
-
+    output = worker.execute(input_data)
+    print '\n<<< Unit Test >>>'
     import pprint
-    pprint.pprint(worker.execute(wrapped_output))
+    pprint.pprint(output)
+
+    # Execute the worker (server test)
+    output = c.work_request('log_meta', md5)
+    print '\n<<< Server Test >>>'
+    import pprint
+    pprint.pprint(output)
 
 if __name__ == "__main__":
     test()
