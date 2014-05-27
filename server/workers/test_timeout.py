137b8100b86158ae636a7e73a54e479dc4d866e0
Brian Wylie <briford.wylie@gmail.com>
2014-05-26 21:28:20 -0600

    adding tests for zerorpc failure cases
    Former-commit-id: 078af1ceb44bddbf19174dab1e5461210439165c

---
+++ server/workers/test_timeout.py
@@ -0,0 +1,44 @@
+
+''' Test: This worker tests for robustly handling timeout situations '''
+import gevent
+
+class Test_Timeout(object):
+    ''' Test: This worker tests for robustly handling timeout situations '''
+    dependencies = []
+
+    def __init__(self):
+        ''' Initialization '''
+        self.sleep_time = 35
+
+    def execute(self, input_data):
+        ''' Test: This worker tests for robustly handling timeout situations '''
+        gevent.sleep(self.sleep_time)
+        return {'timeout':self.sleep_time}
+
+# Unit test: Create the class, the proper input and run the execute() method for a test
+def test():
+    ''' test_timeout.py: Unit test'''
+
+    # This worker test requires a local server running
+    import zerorpc
+    c = zerorpc.Client()
+    c.connect("tcp://127.0.0.1:4242")
+
+    # Execute the worker (unit test)
+    print '\n<<< Unit Test >>>'
+    worker = Test_Timeout()
+    output = worker.execute(None)
+    import pprint
+    pprint.pprint(output)
+
+    # Execute the worker (server test)
+    print '\n<<< Server Test >>>'
+
+    # Execute the worker (server test) (note no unit test as the test is testing server timeouts)
+    try:
+        c.work_request('test_timeout','123')
+    except zerorpc.exceptions.TimeoutExpired:
+        print 'Timeout failed as expected.. so success!'
+
+if __name__ == "__main__":
+    test()
