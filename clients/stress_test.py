56a9f9e2bc99fa18722c6789809ad3e2f9d51c2c
Brian Wylie <briford.wylie@gmail.com>
2014-05-27 12:33:22 -0600

    moving benchmark to stress_test and have the client spin up 8 subprocesses and blast files at workbench
    Former-commit-id: b876089864dbdb3b4d3dcc9c1724709ed72daaef

---
+++ clients/stress_test.py
@@ -0,0 +1,44 @@
+
+''' Benchmark that throws files at workbench with 8 subprocesses '''
+import zerorpc
+import os
+import datetime
+import multiprocessing
+
+def main():
+    ''' Benchmark that throws files at workbench with 8 subprocesses '''
+
+    # Spin off 4 subprocesses
+    pool = multiprocessing.Pool(processes=8)
+    args = ['../data/pe/bad', '../data/pe/good', '../data/pdf/bad', '../data/pdf/good']
+    args += ['../data/pe/bad', '../data/pe/good', '../data/pdf/bad', '../data/pdf/good']
+    pool.map(process_files, args)
+
+def process_files(path):
+    ''' Processes all the files within a directory '''
+    c = zerorpc.Client()
+    c.connect("tcp://127.0.0.1:4242")
+
+    # Benchmark test on shoving data into workbench
+    file_list = [os.path.join(path, child) for child in os.listdir(path)]
+    num_files = len(file_list)
+    total_files = 0
+    start = datetime.datetime.now()
+    for i in xrange(10):
+        for filename in file_list:
+            with open(filename, 'rb') as f:
+                md5 = c.store_sample(filename, f.read(), 'pe')
+                c.work_request('view', md5)
+                print 'Filename: %s' % (filename)
+        total_files += num_files
+    end = datetime.datetime.now()
+    delta = end - start
+    print 'Files processed: %d  Time: %d seconds' % (total_files, delta.seconds)
+
+def test():
+    ''' benchmark test '''
+    main()
+
+if __name__ == '__main__':
+    test()
+
