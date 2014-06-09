07f4e2989b1be4049bff2f6d32618f710fe31685
Brian Wylie <briford.wylie@gmail.com>
2014-06-09 01:06:31 -0600

    okay now loading in the rules through load_rules; also test now does both unit test and server test
    Former-commit-id: be43554f6839fdd12c65ca2b64ad571d6518df4e

--- server/workers/yara_sigs.py
+++ server/workers/yara_sigs.py
@@ -3,6 +3,7 @@
 import os, glob
 import yara
 import datetime
+import inspect
 
 class YaraSigs():
     ''' This worker check for matches against yara sigs. 
@@ -10,27 +11,19 @@ class YaraSigs():
     dependencies = ['sample']
 
     def __init__(self):
-        self.orig_dir = os.getcwd()
         self.rules = self.get_yara_rules()
-        os.chdir(self.orig_dir)
 
     def get_yara_rules(self):
 
         # Try to find the yara rules directory relative to the worker
-        if os.path.exists('yara/rules'):
-            os.chdir('yara/rules')
-        elif os.path.exists('workers/yara/rules'):
-            os.chdir('workers/yara/rules')
-        else:
+        my_dir = os.path.dirname(os.path.realpath(__file__))
+        yara_rule_path = os.path.join(my_dir, 'yara/rules')
+        if not os.path.exists(yara_rule_path):
             raise Exception('yara could not find yara rules directory under: %s' % os.getcwd())
 
-        # Crawl all the .yar and .yara files in the directory and add them to the rules
-        file_list = glob.glob('*.yar')
-        file_list += glob.glob('*.yara')
-        filepaths = {os.path.basename(sig_file):sig_file for sig_file in file_list}
+        # Okay load in all the rules under the yara rule path
+        rules = yara.load_rules(rules_rootpath=yara_rule_path) 
 
-        # Compile all the rules
-        rules = yara.compile(filepaths=filepaths)
         return rules
 
     def execute(self, input_data):
@@ -44,15 +37,42 @@ class YaraSigs():
 # Unit test: Create the class, the proper input and run the execute() method for a test
 def test():
     ''' yara.py: Unit test'''
-    worker = YaraSigs()
 
-    import pprint
+    # This worker test requires a local server running
+    import zerorpc
+    c = zerorpc.Client()
+    c.connect("tcp://127.0.0.1:4242")
+
+    # Store all the files in directory and make an md5 list
     file_list = [os.path.join('../../data/pe/bad', child) for child in os.listdir('../../data/pe/bad')]
-    for test_file in file_list:
-        file_name = os.path.basename(test_file)
-        print '\n%s' % file_name
-        pprint.pprint(worker.execute({'sample':{'raw_bytes':open(test_file, 'rb').read(), 
-                                                'filename': file_name, 'type_tag': 'pe','import_time':datetime.datetime.now()}}))
+    md5_list = []
+    for filename in file_list:
+
+        # Skip OS generated files
+        if '.DS_Store' in filename: continue
+
+        with open(filename,'rb') as file:
+            md5_list.append(c.store_sample(filename, file.read(), 'pe'))
+
+    # Store the md5 list on the server as a sample set
+    sample_set = c.store_sample_set(md5_list)
+
+    # Grab one of the sample for input to the local unit test
+    input_data = c.get_sample(md5_list[0])
+
+    # Execute the worker (unit test)
+    worker = YaraSigs()
+    output = worker.execute(input_data)
+    print '\n<<< Unit Test >>>'
+    import pprint
+    pprint.pprint(output)
+
+    # Execute the worker (server test)
+    output = c.batch_work_request('yara_sigs', {'md5_list': md5_list})
+    get_all_output = list(output)
+    print '\n<<< Server Test >>>'
+    import pprint
+    pprint.pprint(get_all_output)
 
 if __name__ == "__main__":
     test()
