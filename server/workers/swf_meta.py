8d22eeecd124ee0e4ecaf24f922c087c372bfd15
Brian Wylie <briford.wylie@gmail.com>
2014-06-10 15:12:40 -0600

    adding a swf_meta 'stub' worker for now
    Former-commit-id: 02a333aeb5fd6c3feda92109af640a3d1a67cada

---
+++ server/workers/swf_meta.py
@@ -0,0 +1,41 @@
+''' SWFMeta worker: This is a stub the real class (under the experimental 
+                    directory has too many dependencies)
+'''
+
+class SWFMeta():
+    ''' This worker computes a bunch of meta-data about a SWF file '''
+    dependencies = ['sample', 'meta']
+
+    def execute(self, input_data):
+
+        # Get the raw_bytes
+        raw_bytes = input_data['sample']['raw_bytes']
+
+        # Add the meta data to the output
+        output = input_data['meta']
+        return output
+
+# Unit test: Create the class, the proper input and run the execute() method for a test
+def test():
+    ''' swf_meta.py: Unit test'''
+    
+    # This worker test requires a local server running
+    import zerorpc
+    c = zerorpc.Client()
+    c.connect("tcp://127.0.0.1:4242")
+
+    # Generate the input data for this worker
+    md5 = c.store_sample('unknown.swf', open('../../data/swf/unknown.swf', 'rb').read(), 'pe')
+    input_data = c.get_sample(md5)
+    input_data.update(c.work_request('meta', md5))
+
+    # Execute the worker
+    worker = SWFMeta()
+    output = worker.execute(input_data)
+    print 'SWFMeta: '
+    import pprint
+    pprint.pprint(output)
+
+
+if __name__ == "__main__":
+    test()
