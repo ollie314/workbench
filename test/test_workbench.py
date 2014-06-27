0bdc1171c901226e7655e9ca04120b1d5f6737e2
Brian Wylie <briford.wylie@gmail.com>
2014-06-26 18:02:55 -0600

    adding in test dir files

---
+++ test/test_workbench.py
@@ -0,0 +1,19 @@
+"""
+Tests for `workbench` module.
+"""
+import pytest
+from workbench import workbench
+
+
+class TestWorkbench(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
