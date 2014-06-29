407b6d37603fd020f34e53523e5969b75c3ae9ab
Brian Wylie <briford.wylie@gmail.com>
2014-06-29 13:49:44 -0600

    using multiprocess instead of subprocess; doesn't make a difference in coverage but it seems cleaner

--- test/test_workbench.py
+++ test/test_workbench.py
@@ -1,20 +1,16 @@
 """
 Tests for `workbench` module.
 """
-import pytest
-import subprocess
-import time
 import zerorpc
+import multiprocessing
+import workbench.server.workbench as workbench_server
 
 class TestWorkbench(object):
 
-    @classmethod
-    def setup_class(cls):
-        pass
-
     def test_workbench(self):
         # Run the workbench server
-        self.server_process = self.subprocess_manager('workbench')
+        p = multiprocessing.Process(target=workbench_server.run)
+        p.start()
 
         # Start up workbench connection
         workbench = zerorpc.Client()
@@ -33,19 +29,7 @@ class TestWorkbench(object):
 
         # Terminate the workbench server process
         print '\nShutting down the Workbench server...'
-        self.server_process.terminate()
-        time.sleep(3)
-
-    @classmethod
-    def teardown_class(cls):
-        pass
-
-    def subprocess_manager(self, exec_args):
-        try:
-            process = subprocess.Popen(exec_args.split())
-        except OSError:
-            raise RuntimeError('Could not run workbench server (either not installed or not in path): %s' % (exec_args))
-        return process
+        p.terminate()
 
 
 if __name__ == '__main__':
