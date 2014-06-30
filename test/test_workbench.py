80edb61867389fcd82efcd57b1c7958e9ac30c37
Brian Wylie <briford.wylie@gmail.com>
2014-06-29 22:25:03 -0600

    got some monkey patching code for fixing coverage so that it works with gevent

--- test/test_workbench.py
+++ test/test_workbench.py
@@ -1,13 +1,56 @@
 """
 Tests for `workbench` module.
 """
+
 import zerorpc
 import multiprocessing
 import workbench.server.workbench as workbench_server
 
+# Credit for this patching code goes to
+# Vadim Fint (mocksoul)  thanks a bunch you rock
+def patch_coverage_for_gevent():
+    from coverage import collector as _collector, control as _control
+    import collections as _collections
+    import gevent as _gevent
+    _PyTracer = _collector.PyTracer
+
+    class DataStack(object):
+        def __init__(self):
+            self.__data = _collections.defaultdict(list)
+
+        def __idx(self):
+            return hash(_gevent.getcurrent())
+
+        def pop(self):
+            return self.__data[self.__idx()].pop()
+
+        def append(self, value):
+            return self.__data[self.__idx()].append(value)
+
+    class PyTracer(_PyTracer):
+        def __init__(self):
+            _PyTracer.__init__(self)
+            self.data_stack = DataStack()
+
+    _collector.PyTracer = PyTracer
+
+    # Patch coverage, so it uses timid simple tracer by default after monkey patching
+    _coverage = _control.coverage
+
+    class coverage(_coverage):
+        def __init__(self, *args, **kwargs):
+            kwargs.setdefault('timid', True)
+            super(coverage, self).__init__(*args, **kwargs)
+
+    __import__('pyjack').replace_all_refs(_coverage, coverage)
+
+# Monkey path coverage so it works with gevent
+patch_coverage_for_gevent()
+
 class TestWorkbench(object):
 
     def test_workbench(self):
+
         # Run the workbench server
         p = multiprocessing.Process(target=workbench_server.run)
         p.start()
