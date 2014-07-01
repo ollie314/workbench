01f101d2032d6ad8492dd6b851052eb1ab6e6405
Brian Wylie <briford.wylie@gmail.com>
2014-07-01 14:01:17 -0600

    fix code removal error

--- test/test_workbench.py
+++ test/test_workbench.py
@@ -6,7 +6,6 @@ class TestWorkbench(object):
     ''' Test Workbench Server '''
 
     def test_server(self, workbench_conn):
-        patch_coverage_for_gevent()
 
         ''' Test the workbench Server '''
         print '\nStarting up the Workbench server...'
