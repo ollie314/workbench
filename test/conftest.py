0d9385e3eb93df49f6fb7e6b40efc0a1c387358a
Brian Wylie <briford.wylie@gmail.com>
2014-06-30 23:58:09 -0600

    simplifying code by using module based singleton

--- test/conftest.py
+++ test/conftest.py
@@ -3,68 +3,31 @@ Tests for `workbench` module.
 """
 
 import zerorpc
-import multiprocessing
-import workbench.server.workbench as workbench_server
 import pytest
-import time
+import workbench_single
 
-# Fixme: There are lots of bad things in this file need to fix them
+@pytest.fixture(scope='session')
+def workbench_conn(request):
+    ''' Workbench Fixture '''
+    import workbench_single # This spins up the server
 
-# Fixme: Make this a more formal singleton class/method
-process = None
-def start_database():
-    global process
-    if not process:
-        print '\nStarting up the Workbench server...'
-        process = multiprocessing.Process(target=workbench_server.run)
-        process.start()
-    return process
+    # Connect to the server
+    conn = zerorpc.Client(timeout=300)
+    conn.connect('tcp://localhost:4242')
 
-@pytest.yield_fixture(scope='session')
-def workbench_db():
-    ''' Workbench Database  '''
+    # Add a finalize method
+    request.addfinalizer(workbench_single.shutdown)
 
-    # Run the workbench server
-    process = start_database()
-
-    # Start up workbench connection
-    workbench_conn = zerorpc.Client(timeout=300)
-    workbench_conn.connect('tcp://localhost:4242')
-
-    # Hand over the workbench connection to tests who need it
-    yield workbench_conn
-
-    # Close the workbench connection
-    workbench_conn.close()
-
-    # Terminate the workbench server process
-    print '\nShutting down the Workbench server...'
-    process.terminate()
-
-def worker_list():
-    ''' Get the worker list from the workbench database '''
-
-    # Run the workbench server
-    print '\nStarting up the Workbench server...'
-    process = start_database()
-
-    # Start up workbench connection
-    workbench_conn = zerorpc.Client()
-    workbench_conn.connect('tcp://localhost:4242')
-
-    # Get all the workers
-    workers = workbench_conn.list_all_workers()
-
-    # Close the workbench connection
-    workbench_conn.close()
-
-    # Return worker list
-    return workers
+    # Return the connection
+    return conn
 
 def pytest_generate_tests(metafunc):
     if 'worker' in metafunc.fixturenames:
         print 'Getting worker list...'
-        workers = worker_list()
+        # Connect to the server
+        conn = zerorpc.Client()
+        conn.connect('tcp://localhost:4242')
+        workers = conn.list_all_workers()
         metafunc.parametrize('worker', workers)
     else:
         print 'WTF %s' % metafunc.fixturenames
