262037494c354e926cfc1cab6aa549b5573eedb8
Brian Wylie <briford.wylie@gmail.com>
2014-06-30 20:08:12 -0600

    snapshot

--- test/conftest.py
+++ test/conftest.py
@@ -2,10 +2,69 @@
 Tests for `workbench` module.
 """
 
+import zerorpc
+import multiprocessing
+import workbench.server.workbench as workbench_server
+import pytest
+import time
+
+# Fixme: There are lots of bad things in this file need to fix them
+
+# Fixme: Make this a more formal singleton class/method
+process = None
+def start_database():
+    global process
+    if not process:
+        print '\nStarting up the Workbench server...'
+        process = multiprocessing.Process(target=workbench_server.run)
+        process.start()
+    return process
+
+@pytest.yield_fixture(scope='session')
+def workbench_db():
+    ''' Workbench Database  '''
+
+    # Run the workbench server
+    process = start_database()
+
+    # Start up workbench connection
+    workbench_conn = zerorpc.Client(timeout=300)
+    workbench_conn.connect('tcp://localhost:4242')
+
+    # Hand over the workbench connection to tests who need it
+    yield workbench_conn
+
+    # Close the workbench connection
+    workbench_conn.close()
+
+    # Terminate the workbench server process
+    print '\nShutting down the Workbench server...'
+    process.terminate()
+
+def worker_list():
+    ''' Get the worker list from the workbench database '''
+
+    # Run the workbench server
+    print '\nStarting up the Workbench server...'
+    process = start_database()
+
+    # Start up workbench connection
+    workbench_conn = zerorpc.Client()
+    workbench_conn.connect('tcp://localhost:4242')
+
+    # Get all the workers
+    workers = workbench_conn.list_all_workers()
+
+    # Close the workbench connection
+    workbench_conn.close()
+
+    # Return worker list
+    return workers
+
 def pytest_generate_tests(metafunc):
     if 'worker' in metafunc.fixturenames:
         print 'Getting worker list...'
-        workers = metafunc.cls.get_worker_list()
+        workers = worker_list()
         metafunc.parametrize('worker', workers)
     else:
         print 'WTF %s' % metafunc.fixturenames
--- test/test_workbench.py
+++ test/test_workbench.py
@@ -1,98 +1,11 @@
 """
 Tests for `workbench` module.
 """
-
-import zerorpc
-import multiprocessing
-import workbench.server.workbench as workbench_server
-import pytest
-
-# Credit for this patching code goes to
-# Vadim Fint (mocksoul)  thanks a bunch you rock
-def patch_coverage_for_gevent():
-    from coverage import collector as _collector, control as _control
-    import collections as _collections
-    import gevent as _gevent
-    _PyTracer = _collector.PyTracer
-
-    class DataStack(object):
-        def __init__(self):
-            self.__data = _collections.defaultdict(list)
-
-        def __idx(self):
-            return hash(_gevent.getcurrent())
-
-        def pop(self):
-            return self.__data[self.__idx()].pop()
-
-        def append(self, value):
-            return self.__data[self.__idx()].append(value)
-
-    class PyTracer(_PyTracer):
-        def __init__(self):
-            _PyTracer.__init__(self)
-            self.data_stack = DataStack()
-
-    _collector.PyTracer = PyTracer
-
-    # Patch coverage, so it uses timid simple tracer by default after monkey patching
-    _coverage = _control.coverage
-
-    class coverage(_coverage):
-        def __init__(self, *args, **kwargs):
-            kwargs.setdefault('timid', True)
-            super(coverage, self).__init__(*args, **kwargs)
-
-    __import__('pyjack').replace_all_refs(_coverage, coverage)
-
-
+    
 class TestWorkbench(object):
+    ''' Test Workbench Class '''
 
-    # Class variables
-    workbench_conn = None
-    process = None
-
-    @classmethod
-    def setup_class(cls):
-        # Monkey path coverage so it works with gevent
-        # patch_coverage_for_gevent()
-        cls.start_workbench()
-
-    @classmethod
-    def teardown_class(cls):
-        # Close the workbench connection
-        TestWorkbench.workbench_conn.close()
-
-        # Terminate the workbench server process
-        print '\nShutting down the Workbench server...'
-        TestWorkbench.process.terminate()
-
-    @classmethod
-    def start_workbench(cls):
-        if not TestWorkbench.workbench_conn:
-            # Run the workbench server
-            print '\nStarting up the Workbench server...'
-            TestWorkbench.process = multiprocessing.Process(target=workbench_server.run)
-            TestWorkbench.process.start()
-
-            # Start up workbench connection
-            TestWorkbench.workbench_conn = zerorpc.Client(timeout=300)
-            TestWorkbench.workbench_conn.connect('tcp://localhost:4242')
-
-    @classmethod
-    def get_worker_list(cls):
-        # Get a list of workers from the workbench server
-        cls.start_workbench()
-        return TestWorkbench.workbench_conn.list_all_workers()
-
-    def test_worker(self, worker):
+    def test_worker(self, worker, workbench_db):
         # Invoke worker test
         print 'Testing %s...' % worker
-        assert(TestWorkbench.workbench_conn.test_worker(worker))
-
-
-if __name__ == '__main__':
-    test = TestWorkbench()
-    test.setup_class()
-    test.test_worker('view')
-    test.teardown_class()
+        assert workbench_db.test_worker(worker)
