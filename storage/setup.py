fb5a2670d9296d3b4a4be27695d8b8dd20c598d6
Brian Wylie <briford.wylie@gmail.com>
2014-07-05 10:52:34 -0600

    reorganizing setup.py/requirements.txt logic

--- storage/setup.py
+++ storage/setup.py
@@ -1,50 +1,67 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
+import os, sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
 readme = open('README.rst').read()
+long_description = readme
+doclink = '''
+Documentation
+-------------
+
+The full documentation is at http://workbench.rtfd.org. '''
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        errno = tox.cmdline(args=shlex.split(self.tox_args))
+        sys.exit(errno)
+
+# Fixme: Should really look into the community accepted way to do this
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 setup(
-    name='neonx',
-    version='0.1.1',
-    description='Handles conversion of date from NetworkX graph to Neo4j',
-    long_description=readme + '\n\n' + history,
-    author='Rohit Aggarwal',
-    author_email='rohit.neonx@mailnull.com',
-    url='https://github.com/ducky427/neonx',
-    packages=[
-        'neonx',
-    ],
-    package_dir={'neonx': 'neonx'},
+    name='workbench',
+    version='0.1.0',
+    description='A medium-data framework for security research and development teams.',
+    long_description=readme + '\n\n' + doclink + '\n\n' + history,
+    author='Brian Wylie',
+    author_email='briford@supercowpowers.com',
+    url='https://github.com/SuperCowPowers/workbench',
+    packages=['workbench','workbench.clients', 'workbench.server',
+	      'workbench.server.bro', 'workbench.workers','workbench.utils'],
+    package_dir={'workbench': 'workbench'},
     include_package_data=True,
-    install_requires=['networkx', 'requests'
-    ],
-    license="MIT",
+    scripts = ['workbench/workbench'],
+    tests_require=['tox'],
+    cmdclass = {'test': Tox},
+    install_requires=[required],
+    license='MIT',
     zip_safe=False,
-    keywords='neonx',
+    keywords='workbench, security, python',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-    ],
-    test_suite='tests',
+    ]
 )
