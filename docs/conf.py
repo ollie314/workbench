c4c495c6a2b4554a279004d076b70e22818108f5
iamkhush <ankush.chadda@gmail.com>
2014-07-06 01:29:51 +0530

    Removing mozilla, adding sphinx-napolean for autodocs

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
