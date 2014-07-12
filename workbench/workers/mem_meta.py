40e2c19e324ea461d014a33699edb1d5734fe5d0
Brian Wylie <briford.wylie@gmail.com>
2014-07-12 16:33:25 -0600

    okay reorganizing the way the rekall tests are done; also excluded two of them for now

--- workbench/workers/mem_meta.py
+++ workbench/workers/mem_meta.py
@@ -4,6 +4,7 @@
     All credit for good stuff goes to them, all credit for bad stuff goes to us. :)
 '''
 import os
+import hashlib
 import mem_base
 
 class MemoryImageMeta(mem_base.MemoryImageBase):
@@ -23,26 +24,27 @@ def test():
 
     # This worker test requires a local server running
     import zerorpc
-    c = zerorpc.Client(timeout=120)
-    c.connect("tcp://127.0.0.1:4242")
+    workbench = zerorpc.Client(timeout=120)
+    workbench.connect("tcp://127.0.0.1:4242")
 
     # Store the sample
     data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data/memory_images/exemplar4.vmem')
-    md5 = c.store_sample('exemplar4.vmem', open(data_path, 'rb').read(), 'mem')
-
-    # Unit test stuff
-    input_data = c.get_sample(md5)
+    with open(data_path, 'rb') as mem_file:
+        raw_bytes = mem_file.read()
+        md5 = hashlib.md5(raw_bytes).hexdigest()
+        if not workbench.has_sample(md5):
+            md5 = workbenchstore_sample('exemplar4.vmem', open(data_path, 'rb').read(), 'mem')
 
     # Execute the worker (unit test)
     worker = MemoryImageMeta()
-    output = worker.execute(input_data)
+    output = worker.execute({'sample':{'raw_bytes':raw_bytes}})
     print '\n<<< Unit Test >>>'
     import pprint
     pprint.pprint(output)
     assert 'Error' not in output
 
     # Execute the worker (server test)
-    output = c.work_request('mem_meta', md5)
+    output = workbench.work_request('mem_meta', md5)
     print '\n<<< Server Test >>>'
     import pprint
     pprint.pprint(output)
--- workbench/workers/mem_procdump.py
+++ workbench/workers/mem_procdump.py
@@ -10,6 +10,7 @@ import contextlib
 import tempfile
 import shutil
 import glob, os
+import hashlib
 
 class MemoryImageProcDump(object):
     ''' This worker dumps process pe files from memory image files. '''
@@ -86,32 +87,33 @@ class MemoryImageProcDump(object):
 
 # Unit test: Create the class, the proper input and run the execute() method for a test
 import pytest
-@pytest.mark.xfail
+@pytest.mark.exclude
 def test():
     ''' mem_procdump.py: Test '''
 
     # This worker test requires a local server running
     import zerorpc
-    c = zerorpc.Client(timeout=120)
-    c.connect("tcp://127.0.0.1:4242")
+    workbench = zerorpc.Client(timeout=120)
+    workbench.connect("tcp://127.0.0.1:4242")
 
     # Store the sample
     data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data/memory_images/exemplar4.vmem')
-    md5 = c.store_sample('exemplar4.vmem', open(data_path, 'rb').read(), 'mem')
-
-    # Unit test stuff
-    input_data = c.get_sample(md5)
+    with open(data_path, 'rb') as mem_file:
+        raw_bytes = mem_file.read()
+        md5 = hashlib.md5(raw_bytes).hexdigest()
+        if not workbench.has_sample(md5):
+            md5 = workbenchstore_sample('exemplar4.vmem', open(data_path, 'rb').read(), 'mem')
 
     # Execute the worker (unit test)
     worker = MemoryImageProcDump()
-    output = worker.execute(input_data)
+    output = worker.execute({'sample':{'raw_bytes':raw_bytes}})
     print '\n<<< Unit Test >>>'
     import pprint
     pprint.pprint(output)
     assert 'Error' not in output
 
     # Execute the worker (server test)
-    output = c.work_request('mem_procdump', md5)
+    output = workbench.work_request('mem_procdump', md5)
     print '\n<<< Server Test >>>'
     import pprint
     pprint.pprint(output)
