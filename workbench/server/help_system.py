51230a82f2bb321285aae4b1018fed13161c50bb
Brian Wylie <briford.wylie@gmail.com>
2014-07-17 13:06:32 -0600

    adding a separate help system module

---
+++ workbench/server/help_system.py
@@ -0,0 +1,103 @@
+
+""" Workbench Help System """
+
+import inspect
+import funcsigs
+
+class HelpSystem(object):
+    """ Workbench Help System """
+    __version__ = 0.1
+
+    def __init__(self, workbench_instance):
+        """Initialize the Help System.
+
+        Args:
+            workbench_instance: an instance of a workbench class so that 
+                                we can make introspection calls
+        """
+        
+        # Set workbench instance
+        self.my_wb = workbench_instance
+
+        # Announce Version
+        print '<<< Workbench Help System %s >>>' % self.__version__
+
+    def help(self):
+        """ Returns help commands """
+
+        help_str =  '\nWelcome to Workbench: Here\'s a list of help commands:'
+        help_str += '\n\t - Run workbench.help_basic() for beginner help'
+        help_str += '\n\t - Run workbench.help_commands() for command help'
+        help_str += '\n\t - Run workbench.help_workers() for a list of workers'
+        help_str += '\n\t - Run workbench.help_advanced() for advanced help'
+        help_str += '\n\nSee http://github.com/SuperCowPowers/workbench for more information'
+        return help_str
+
+    def help_basic(self):
+        """ Returns basic help commands """
+        help_str =  '\nWorkbench: Getting started...'
+        help_str += '\n\t - 1) $ print workbench.help_commands() for a list of commands'
+        help_str += '\n\t - 2) $ print workbench.help_command(\'store_sample\') for into on a specific command'
+        help_str += '\n\t - 3) $ print workbench.help_workers() for a list a workers'
+        help_str += '\n\t - 4) $ print workbench.help_worker(\'meta\') for info on a specific worker'
+        help_str += '\n\t - 5) $ my_md5 = workbench.store_sample(...)'
+        help_str += '\n\t - 6) $ output = workbench.work_request(\'meta\', my_md5)'
+        return help_str
+
+    def help_commands(self):
+        """ Returns a big string of Workbench commands and signatures """
+        help_string = 'Workbench Commands:'
+        for name, meth in inspect.getmembers(self.my_wb, predicate=inspect.ismethod):
+            if not name.startswith('_'):
+                help_string += '\n\t%s%s' % (name,funcsigs.signature(meth))
+        return help_string
+
+    def help_command(self, command):
+        """ Returns a specific Workbench command and docstring """
+        for name, meth in inspect.getmembers(self.my_wb, predicate=inspect.ismethod):
+            if name == command:
+                return '\n Command: %s%s \n%s' % (name, funcsigs.signature(meth), meth.__doc__)
+        return '%s command not found.. misspelled?' % command
+
+    def help_workers(self):
+        """ Returns a big string of the loaded Workbench workers and their dependencies """
+        help_string = 'Workbench Workers:'
+        for name, plugin in sorted(self.my_wb.plugin_meta.iteritems()):
+            help_string += '\n\t%s %s' % (name, str(plugin['class'].dependencies))
+        return help_string
+
+    def help_worker(self, worker):
+        """ Returns a specific Workbench worker and docstring """
+        try:
+            plugin = self.my_wb.plugin_meta[worker]
+            return '\n Worker: %s %s\n\t%s' % (worker, str(plugin['class'].dependencies), plugin['class'].__doc__)
+        except KeyError:
+            return '%s worker not found.. misspelled?' % worker
+
+    def help_advanced(self):
+        """ Returns advanced help commands """
+        help_str =  '\nWoo! Advanced... <fixme: add documentation for advanced> :)'
+        help_str += '\n\nSee http://github.com/SuperCowPowers/workbench for more information'
+        return help_str
+
+    def help_everything(self):
+        """Executes help_system.py test."""
+    
+        # Call all the methods for the help system
+        output = self.help()
+        output += self.help_advanced()
+        output += self.help_basic()
+    
+        # Yes this is verbose but useful to see what commands
+        # have proper docstrings and make sure help works, etc
+        output += self.help_commands()
+        for command in self.my_wb.list_all_commands():
+            output += self.help_command(command)
+    
+        # Yes this is verbose but useful to see what workers
+        # have proper docstrings and make sure help works, etc
+        output += self.help_workers()
+        for worker in self.my_wb.list_all_workers():
+            output += self.help_worker(worker)
+
+        return output
