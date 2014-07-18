411707c0e4bda5c861bdc5e137c9c8c1569a050c
iamkhush <ankush.chadda@gmail.com>
2014-07-18 15:09:39 +0530

    adding other modules to mock so that import doesnt cause issues #162

--- docs/conf.py
+++ docs/conf.py
@@ -14,11 +14,33 @@
 
 import sys
 import os
-import mock
+
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__','event'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
  
-MOCK_MODULES = ['cython', 'yara', 'zmq', 'yaml', 'gevent']
+MOCK_MODULES = ['cython', 'yara', 'zmq', 'yaml', 'gevent', 'ssdeep', 'amd64', 
+                'gevent.event']
 for mod_name in MOCK_MODULES:
-  sys.modules[mod_name] = mock.Mock()
+  sys.modules[mod_name] = Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
