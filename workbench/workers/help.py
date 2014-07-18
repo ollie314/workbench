5be0eb0fe379288ccc95b1a3299125929d9af5be
Brian Wylie <briford.wylie@gmail.com>
2014-07-18 16:38:44 -0600

    adding a help worker

---
+++ workbench/workers/help.py
@@ -0,0 +1,36 @@
+
+''' Help worker '''
+
+class Help(object):
+    ''' This worker computes help for a worker '''
+    dependencies = ['worker_info']
+
+    def execute(self, input_data):
+        fields = ['name', 'doc', 'dependencies', 'mod_time']
+        return {'output': {key:value for key,value in input_data['worker_info'].iteritems() if key in fields}}
+
+# Unit test: Create the class, the proper input and run the execute() method for a test
+def test():
+    ''' help.py: Unit test'''
+
+    # This worker test requires a local server running
+    import zerorpc
+    workbench = zerorpc.Client(timeout=300, heartbeat=60)
+    workbench.connect("tcp://127.0.0.1:4242")
+
+    # Generate input for the worker
+    input_data = workbench.worker_info('meta')
+
+    # Execute the worker (unit test)
+    worker = Help()
+    output = worker.execute(input_data)
+    print '\n<<< Unit Test >>>'
+    print output['output']
+
+    # Execute the worker (server test)
+    output = workbench.work_request('help', 'meta')
+    print '\n<<< Server Test >>>'
+    print output['help']['output']
+
+if __name__ == "__main__":
+    test()
