44865beccdd5d416e31f6115740abc22ca4f9859
iamkhush <ankush.chadda@gmail.com>
2014-07-18 13:32:19 +0530

    Trying to remove the cython error on rtd #150

--- docs/conf.py
+++ docs/conf.py
@@ -14,13 +14,18 @@
 
 import sys
 import os
+import mock
+ 
+MOCK_MODULES = ['cython']
+for mod_name in MOCK_MODULES:
+  sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../../workbench/'))
-# sys.path.insert(0, os.path.abspath('../../workbench/clients/'))
-# sys.path.insert(0, os.path.abspath('../../workbench/server/'))
+sys.path.insert(0, os.path.abspath('../../workbench/clients/'))
+sys.path.insert(0, os.path.abspath('../../workbench/server/'))
 
 # -- General configuration ------------------------------------------------
 
