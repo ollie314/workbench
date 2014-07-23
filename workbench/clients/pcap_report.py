835167fbe277cefef5786501503441bf3694ad67
iamkhush <ankush.chadda@gmail.com>
2014-07-24 02:59:42 +0530

    Making pcap_report.py a flask app.

--- workbench/clients/pcap_report.py
+++ workbench/clients/pcap_report.py
@@ -4,7 +4,11 @@ import zerorpc
 import os
 import pprint
 import workbench_client
+import flask
 
+app = flask.Flask(__name__)
+
+@app.route('/')
 def run():
     """This client pulls PCAP 'views' (view summarize what's in a sample)."""
     
@@ -15,9 +19,6 @@ def run():
     workbench = zerorpc.Client(timeout=300, heartbeat=60)
     workbench.connect('tcp://'+args['server']+':'+args['port'])
 
-    # Test out getting the raw Bro logs from a PCAP file
-    # Note: you can get a super nice 'generator' python list of dict by using
-    #       'stream_sample' instead of 'get_sample'.
     data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'../data/pcap')
     file_list = [os.path.join(data_path, child) for child in os.listdir(data_path)]
     for filename in file_list:
@@ -29,13 +30,12 @@ def run():
         with open(filename,'rb') as f:
             md5 = workbench.store_sample(filename, f.read(), 'pcap')
             results = workbench.work_request('view_pcap', md5)
-            print '\n<<< %s >>>' % filename
-            pprint.pprint(results)
+    return flask.render_template('../data/templates/index.html', results=results)            
 
 def test():
     ''' pcap_bro_view test '''
     run()
 
 if __name__ == '__main__':
-    run()
+    app.run(host='0.0.0.0', debug=True)
 
