70c5b56415a2d412628f7223296af7846c9b002d
iamkhush <ankush.chadda@gmail.com>
2014-07-26 17:23:16 +0530

    Adding beautiful design #135

--- workbench/clients/pcap_report.py
+++ workbench/clients/pcap_report.py
@@ -6,21 +6,28 @@ import pprint
 import workbench_client
 import flask
 
-app = flask.Flask(__name__)
+STATIC_DIR = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), '../data/')
 
-@app.route('/')
+APP = flask.Flask(__name__, template_folder=STATIC_DIR,
+                  static_folder=STATIC_DIR, static_url_path='')
+
+@APP.route('/')
 def run():
     """This client pulls PCAP 'views' (view summarize what's in a sample)."""
     
-    # Grab server args
+    # Grab grab_server_argsrver args
     args = workbench_client.grab_server_args()
 
     # Start up workbench connection
     workbench = zerorpc.Client(timeout=300, heartbeat=60)
     workbench.connect('tcp://'+args['server']+':'+args['port'])
 
-    data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'../data/pcap')
-    file_list = [os.path.join(data_path, child) for child in os.listdir(data_path)]
+    data_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), '../data/pcap')
+    file_list = [os.path.join(data_path, child) for child in \
+                os.listdir(data_path)]
+    results = []
     for filename in file_list:
 
         # Skip OS generated files
@@ -29,13 +36,19 @@ def run():
         # Process the pcap file
         with open(filename,'rb') as f:
             md5 = workbench.store_sample(filename, f.read(), 'pcap')
-            results = workbench.work_request('view_pcap', md5)
-    return flask.render_template('../data/templates/index.html', results=results)            
+            results.append([filename.split('/')[-1], workbench.work_request('view_pcap', md5)])
+
+    return results
+
+def flask_app():
+    '''Return redered template for the flask app'''
+    results = run()
+    return flask.render_template('templates/index.html', results=results)            
 
 def test():
     ''' pcap_bro_view test '''
     run()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    APP.flask_app(host='0.0.0.0', debug=True)
 
