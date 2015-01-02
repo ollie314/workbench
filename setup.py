2b420298d91e4bbcdcf086d98d90ede48b40a63e
Brian Wylie <briford.wylie@gmail.com>
2015-01-02 13:25:18 -0700

    a bunch of setup/release changes

--- setup.py
+++ setup.py
@@ -5,10 +5,6 @@ import os, sys
 from setuptools import setup
 
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
-
 readme = open('README.rst').read()
 long_description = readme
 doclink = '''
